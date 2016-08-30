b360e31c6bc5d341d8cedace3d579405b1c6cdd1
Eric W. Greene <eric@ericwgreene.com>
2016-08-30 14:16:15 -0700

    added input code

--- src/www/js/app.js
+++ src/www/js/app.js
@@ -4,12 +4,48 @@ import '../css/styles.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class AppComponent extends React.Component {
+class ItemList extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			newColor: ''
+		};
+
+		this.onChange = this.onChange.bind(this);
+	}
+
+	onChange(e) {
+
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+
+	}
 
 	render() {
-		return <h1>React/Flux/Redux/Relay/GraphQL Starter Project!!!</h1>;
+
+		return <div>
+			<h1>{this.props.headerText}</h1>
+			<ul>
+				{this.props.items.map((item, index) =>
+					<li key={index}>{item}</li>
+				)}
+			</ul>
+			<form>
+				<label htmlFor="newColor">New Color:</label>
+				<input type="text" name="newColor" id="newColor"
+					value={this.state.newColor} onChange={this.onChange} />
+				<button type="button">Add Color</button>
+			</form>
+		</div>;
 	}
 
 }
 
-ReactDOM.render(<AppComponent />, document.querySelector('main'));
+const colors = ['red','white','gold','green','saffron','blue'];
+
+ReactDOM.render(<ItemList headerText="Natonal Colors" items={colors} />, document.querySelector('main'));
+
+
